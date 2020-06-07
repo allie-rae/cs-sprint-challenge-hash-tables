3c0bc5fda767df6f5293fffc95a997003751a623
Allie Robinson <allierob93@gmail.com>
2020-06-06 21:22:54 -0500

    completed ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -4,11 +4,29 @@ class Ticket:
         self.source = source
         self.destination = destination
 
-
+# tickets is an array of string variable names
+# length is the number of tickets
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
 
+    dict = {}
+    route = []
+
+    # for each ticket in the tickets array
+    for ticket in tickets:
+        # store the source as the key and the destination 
+        # as the value in the dict
+        dict[ticket.source] = ticket.destination
+    # the first source is "NONE", so the pointer begins
+    # at the key whose value is "NONE"
+    pointer = dict["NONE"]
+    # this needs to run until all tickets have been arranged:
+    # length = number of tickets
+    while len(route) < length:
+        # append the pointer to the array
+        route.append(pointer)
+        # change the pointer to the value of the key:value pair,
+        # which is the destination of the ticket instead of the source
+        pointer = dict[pointer]
     return route
+    
+
