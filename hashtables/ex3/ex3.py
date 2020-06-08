ddf498580496fbb82f5f264ad06193352643830d
Allie Robinson <allierob93@gmail.com>
2020-06-08 02:57:56 -0500

    cleaned up code

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -31,11 +31,9 @@ def intersection(arrays):
 if __name__ == "__main__":
     arrays = []
 
-    # arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    # arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    # arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
-    arrays.append(list(range(30, 40)) + [1, 2, 3])
-    arrays.append(list(range(40, 50)) + [1, 2, 3])
-    arrays.append(list(range(25, 35)) + [1, 2, 3])
+    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+
 
     print(intersection(arrays))
