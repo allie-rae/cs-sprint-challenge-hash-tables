9e4530b60efce21004a3503d94bf0cc6ce966240
Allie Robinson <allierob93@gmail.com>
2020-06-08 05:57:22 -0500

    ex4 complete

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,9 +1,15 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    dict = {}
+    result = []
 
+    # for each number in the array
+    for num in a:
+        # if it's opposite exists in the array (positive or negative)
+        if -num in dict:
+            # append it to the results array
+            result.append(num if num >= 0 else -num)
+        # add all numbers to the dict 
+        dict[num] = 1
     return result
 
 
